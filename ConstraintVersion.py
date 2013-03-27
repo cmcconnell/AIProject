6092590671cbe5d3e56377f8f9ef1aeb72258da9
Chris McConnell <mcconnell.c@husky.neu.edu>
2013-03-27 12:29:36 -0400

    Making initial python files

---
+++ ConstraintVersion.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+"""
+This will be the version of our project using Constraint Propogation to solve sudoku puzzles"""
+
---
+++ RulesVersion.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+""" This will be the version of our project using a knowledge base of rules to solve sudoku puzzles"""
