5cb8ed97fe1a34c84cedce64804b62ed3ae5724d
Nick Picone <nicholas.picone@gmail.com>
2013-04-03 09:03:18 -0400

    Added code for first rule

--- RulesVersion.py
+++ RulesVersion.py
@@ -1,24 +1,13 @@
 """
 Foundations of Artificial Intelligence, Spring 2013
 
-This version of our program applies more strategy rules to narrow down the
-number of possible solutions than the constraint propogation version and
-then searches through the smaller search space if it still hasn't found the
-solution.
+This version of our program takes a rules-based approach to solving sudoku
+puzzles. It applies strategy rules to fill in blanks in the sudoku puzzle
+like a person solving the puzzle would.
 
 Sources:
 Peter Norvig's program -  http://norvig.com/sudopy.shtml
 Sudoku strategies - http://www.sudokudragon.com/sudokustrategy.htm
-"""
-
-"""
-Norvig's approach is the opposite of the human approach. The program
-starts with all possibilities for each square and eliminates as those
-are proven wrong, while a human starts with blank squares and fills in numbers
-as they are found.
-
-Thinking about which way to approach it using rules and how much of Norvig's
-to reuse.
 """
 
 ## Throughout this program we have:
@@ -66,63 +55,33 @@ def test():
 
 ################ Parse a Grid ################
 
-def parse_grid(grid):
-    """Convert grid to a dict of possible values, {square: digits}, or
-    return False if a contradiction is detected."""
-    ## To start, every square can be any digit; then assign values from the grid.
-    values = dict((s, digits) for s in squares)
-    for s,d in grid_values(grid).items():
-        if d in digits and not assign(values, s, d):
-            return False ## (Fail if we can't assign d to square s.)
-    return values
-
 def grid_values(grid):
     "Convert grid into a dict of {square: char} with '0' or '.' for empties."
     chars = [c for c in grid if c in digits or c in '0.']
     assert len(chars) == 81
     return dict(zip(squares, chars))
 
-################ First few rules ####################
+################ Apply rules ####################
 
-# The constraint propogation part of Norvig's program corresponds to the first
-#  few rules on Sudoku Dragon. The only choice rule corresponds to (2) below,
-#  and the single possibility rule, the only square rule, and the two out of
-#  three rule are based on (1).
+def empty_squares(values):
+    "Returns a list of the empty squares in values"
+    return [s for s in squares if values[s] in '0.']
 
-def assign(values, s, d):
-    """Eliminate all the other values (except d) from values[s] and propagate.
-    Return values, except return False if a contradiction is detected."""
-    other_values = values[s].replace(d, '')
-    if all(eliminate(values, s, d2) for d2 in other_values):
-        return values
-    else:
-        return False
+# for each empty square, apply the rules in order; return false if grid doesn't change
 
-def eliminate(values, s, d):
-    """Eliminate d from values[s]; propagate when values or places <= 2.
-    Return values, except return False if a contradiction is detected."""
-    if d not in values[s]:
-        return values ## Already eliminated
-    values[s] = values[s].replace(d,'')
-    ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
-    if len(values[s]) == 0:
-        return False ## Contradiction: removed last value
-    elif len(values[s]) == 1:
-        d2 = values[s]
-        if not all(eliminate(values, s2, d2) for s2 in peers[s]):
-            return False
-    ## (2) If a unit u is reduced to only one place for a value d, then put it there.
-    for u in units[s]:
-        dplaces = [s for s in u if d in values[s]]
-        if len(dplaces) == 0:
-            return False ## Contradiction: no place for this value
-        elif len(dplaces) == 1:
-            # d can only be in one place in unit; assign it there
-            if not assign(values, dplaces[0], d):
-                return False
-    return values
-
-################ More rules #########################
+def only_choice(values, empty):
+    for s in empty:
+        for unit in units[s]:
+            i = 0
+            other_numbers = []
+            for u in unit:
+                if values[u] not in '0.':
+                    i += 1
+                    other_numbers.append(values[u])
+            if i == 8:
+                v = [n for n in digits if n not in other_numbers]
+                values[s] = v
+                empty.remove(s)
 
 ################ Display as 2-D grid ################
 
