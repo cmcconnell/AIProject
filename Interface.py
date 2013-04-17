d89e7584ddb39b1a15044a2f0cfdd4b0e9508519
Vishwanath Pai <vishwa@ubuntu.(none)>
2013-04-16 23:12:53 -0400

    Added documentation

--- Interface.py
+++ Interface.py
@@ -14,6 +14,11 @@ from random import *
 from RulesVersion import * 
 import ConstraintVersion
 
+#Method: ui_cross
+#Short Desc: Same as cross method in ConstraintsVersion.py
+#Param1: A list
+#Param2: A list
+#Return: List of AxB
 def ui_cross(A, B):
     "Cross product of elements in A and elements in B."
     return [a+b for a in A for b in B]
@@ -35,6 +40,11 @@ class MainWindow:
     values = {}
     solver = ''
     grid = ''
+    
+    #Method: displayDialog
+    #Short Desc: Displays message in a dislog box
+    #Param1: 'A Message to display'
+    #Return: None
     def displayDialog(self,message):
         md = gtk.MessageDialog(self.window,
                                0, gtk.MESSAGE_INFO,
@@ -42,20 +52,29 @@ class MainWindow:
         md.run()
         md.destroy()
 
-    # This callback quits the program
+    #This callback quits the program
     def delete_event(self, widget, event, data=None):
         gtk.main_quit()
         return False
     
+    #Just a place holder, does nothing.
     def square_callback(self, widget, data=None):
         pass
-        
+    
+    #Just a place holder, does nothing.   
     def insert_callback(self, widget, data=None):
         pass
-        
+    
+    #Method: solve_callback
+    #Short Desc: Event handler for 'Solve' button click. 
+    #Param1: The widget which raised the event
+    #Param2: Any data that needs to be passed to the function
+    #Return: None    
     def solve_callback(self, widget, data=None):
+        #To calculate total time of execution
         startTime = time.time()
         
+        #Based on the value of the radio button call constraints or rules version
         if(self.radio_button_constraints.get_active()):
             ret = self.solve_constraints()
         else:
@@ -64,32 +83,60 @@ class MainWindow:
         endTime = time.time()
         duration = (endTime - startTime) * 1000
             
+        #Display error if solver returns False
         if(ret == False):
             self.displayDialog('Could not solve the puzzle')
             return
         
+        #Display time on the label next to 'Reset'
         if ret != 'EINVAL':
             self.status_label.set_text("Time: " + str(duration) + " ms")        
-        
-        
+      
+    #Method: reset_callback
+    #Short Desc: Event handler for 'Reset' button click. 
+    #Param1: The widget which raised the event
+    #Param2: Any data that needs to be passed to the function
+    #Return: None    
     def reset_callback(self, widget, data=None):
+        #Blank out all labels of the squares
         for square in ui_squares:
             self.buttons[square].set_label("       ")
         
+        #Reset all other data
         self.values = {}
         self.grid = ''
         self.status_label.set_text('')
         
+    #Method: set_square
+    #Short Desc: Assign value to a particular square (display the value) 
+    #Param1: Square
+    #Param2: Digit
+    #Return: None
     def set_square(self, square, value):
         self.buttons[square].set_label("   " + str(value) + "   ")
         
+    #Method: open_callback
+    #Short Desc: Event handler for 'Open' button click. 
+    #Param1: The widget which raised the event
+    #Param2: Any data that needs to be passed to the function
+    #Return: None
     def open_callback(self, widget, data=None):
         self.filesel.show()
         
+    #Method: file_ok_sel
+    #Short Desc: Event handler for 'Ok' button click in the file select window 
+    #Param1: The widget which raised the event
+    #Return: None
     def file_ok_sel(self, w):
+        #Display the selected file name in the text box
         self.entry.set_text(self.filesel.get_filename())
+        
+        #Hide the file select dialog
         self.filesel.hide()
+        
+        #Try to open the file and read
         try:
+            #Open file in read only mode
             file = open(self.entry.get_text(),"r")
             tmp = file.read()
             if(len(tmp) == 0):
@@ -97,19 +144,28 @@ class MainWindow:
                 return False
             
             grid = ''
+            
+            #Parse only digits and dots. Ignore all else
             for i in tmp:
-                if i.isdigit():
+                if i.isdigit() or i == '.':
                     grid = grid + i
                     
             self.grid = grid
+            
+            #Get the values from Solver (parse the grid)
             self.solver = Solver(self.grid)
             self.values = self.solver.grid_values()
-            #print self.values
+            
+            #Display the puzzle on screen
             self.display_sudoku(self.values)
         except IOError, e:
             print "Error opening file ({0}): {1}".format(e.errno, e.strerror)
             return False
             
+    #Method: display_sudoku
+    #Short Desc: Display values on screen 
+    #Param1: Values dictionary
+    #Return: None
     def display_sudoku(self, values):
         keys = values.keys()
         
@@ -120,7 +176,7 @@ class MainWindow:
         # Create a new window
         self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 
-        # Set the window title
+        # Set the window title The widget which raised the eventand set resizable to False
         self.window.set_title("SuDoKu")
         self.window.set_resizable(False)
 
@@ -129,38 +185,45 @@ class MainWindow:
         self.window.connect("delete_event", self.delete_event)
 
         # Sets the border width of the window.
-        self.window.set_border_width(20)
+        self.window.set_border_width(20)        
         
-        
+        #Set event handlers for OK and Cancel buttons in the file selector
         self.filesel.ok_button.connect("clicked", self.file_ok_sel)
         self.filesel.cancel_button.connect("clicked", lambda w: self.filesel.hide())
         
+        #Create a vertical box that holds all the widgets in the screen
         vbox = gtk.VBox(False, spacing=10)
         
-        # Put the table in the main window
+        #Add the vbox to the window (all other widgets will be added to this vbox
         self.window.add(vbox)  
         
-        #vbox.pack_start(child, expand=False, fill=True, padding=0)
-
-        # Create a 12x14 table
+        # Create a 12x14 table which holds the sudoku grid
         sudoku_grid = gtk.Table(12, 14, False) 
         
+        #Buttons holds all the buttons on the grid. This is helpful to populate
+        #them with values
         self.buttons = {}
         
+        #For each square create a button on the grid
         for square in ui_squares:
+            #Calculate the row and column in which each square goes 
+            #(we translate A,B,C etc to 1,2,3... for row numbers)
             row = int(alpha_to_numbers[square[0]])
             column = int(square[1])
             
+            #Row 4,8 are filled with '|'
             if (row > 3 and row < 7):
                 row += 1
             elif (row > 6):
                 row += 2
-                
+            
+            #Column 4,8 are filled with '----'    
             if (column > 3 and column < 7):
                 column += 1
             elif (column > 6):
                 column += 2
-                
+                 
+            #Create new button and attach it to the table
             button = gtk.ToggleButton(square)
             button.set_label("       ")
             button.connect("toggled", self.square_callback, square)
@@ -169,6 +232,7 @@ class MainWindow:
         
             self.buttons[square] = button
             
+        #Add '------' for column 5,8
         for i in range (1,10):
             if not (i == 4 or i == 8):
                 continue
@@ -178,7 +242,8 @@ class MainWindow:
                 label = gtk.Label("-------")
                 sudoku_grid.attach(label, j - 1, j, i - 1, i)
                 label.show()
-                
+           
+        #Add '|' for row 4,8    
         for i in range (1,10):
             if not (i == 4 or i == 8):
                 continue
@@ -186,21 +251,13 @@ class MainWindow:
                 label = gtk.Label("|")
                 sudoku_grid.attach(label, i - 1, i, j - 1, j)
                 label.show()
-                
-        for i in range(1,10):
-            row = i
-            column = 14
-            button = gtk.Button(square)
-            button.set_label("   " + str(i) + "   ")
-            button.connect("clicked", self.insert_callback, row)
-            sudoku_grid.attach(button, column - 1, column, row - 1, row)
-            #button.show()
         
+        #Show the grid and pack it into the vbox
         sudoku_grid.show()
         vbox.show()
         vbox.pack_start(sudoku_grid, False, False, padding=0)
         
-        #Add check boxes here
+        #Add radio buttons
         hbox = gtk.HBox(False,10)
         hbox.show()
         
@@ -216,6 +273,7 @@ class MainWindow:
         
         vbox.pack_start(hbox, False, False, padding=0)
         
+        #Display the file selection widgets
         file_grid = gtk.Table(1, 3, False)
         
         label = gtk.Label("Select input file: ")
@@ -238,6 +296,7 @@ class MainWindow:
         hbox = gtk.HBox(False,10)
         hbox.show()
         
+        #Display the Solve, Reset buttons and the Time display label
         button = gtk.Button("Solve")
         button.set_label("Solve")
         button.connect("clicked", self.solve_callback, "Solve")
@@ -260,6 +319,10 @@ class MainWindow:
         
         self.window.show()
         
+    #Method: solve_constraints
+    #Short Desc: Solve puzzle using constraints version
+    #Param1: None
+    #Return: 'EINVAL' on invalid file and False if puzzle couldn't be solved
     def solve_constraints(self):
         ret = True
         if self.grid == '':
@@ -274,6 +337,10 @@ class MainWindow:
         else:
             return False
     
+    #Method: solve_constraints
+    #Short Desc: Solve puzzle using rules version
+    #Param1: None
+    #Return: 'EINVAL' on invalid file and False if puzzle couldn't be solved
     def solve_rules(self):
         ret = True
         if self.grid == '':
@@ -287,7 +354,7 @@ class MainWindow:
         self.display_sudoku(self.values)        
         return ret
     
-
+#Main function. Starts the UI.
 def main():
     grid1  = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
     grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
