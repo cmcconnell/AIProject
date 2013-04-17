6c3384e8a0b255f380952970d2e19ca63ff4c298
Nick Picone <nicholas.picone@gmail.com>
2013-04-17 08:04:40 -0400

    Cleaned up Sudoku.py, removed unused files

--- Sudoku.py
+++ Sudoku.py
@@ -1,35 +1,16 @@
 """
 Foundations of Artificial Intelligence, Spring 2013
 
-This version of our program takes a rules-based approach to solving sudoku
-puzzles. It applies strategy rules to fill in blanks in the sudoku puzzle
-like a person solving the puzzle would.
+The main file to start the solver program with the interface.
 
-Sources:
-Peter Norvig's program -  http://norvig.com/sudopy.shtml
-Sudoku strategies - http://www.sudokudragon.com/sudokustrategy.htm
 """
 
 from Interface import *
 
-def run_test():
-    filename = 'sudoku'
-    file = open(filename)
-    grid = file.read().strip()
-    solver = Solver(grid)
-    values = solver.grid_values()
-    
-    print "-----------------------------initial------------------------------"
-    solver.display(values)
-    
-    solver.solve(values)
-    
-    print "---------------------------after solving--------------------------"
-    solver.display(values)
 def run():
     MainWindow()
     gtk.main()
-    return 0
+
 if __name__ == '__main__':
     run()
 
